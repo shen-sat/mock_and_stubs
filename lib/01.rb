7beb04824f844ea4f67bff4eb622c5aebcbfb31b
Shen Satkunarasa <shenthuran.satkunarasa@fundingcircle.com>
2023-09-06 09:15:42 +0100

    First commit

---
+++ lib/01.rb
@@ -0,0 +1,25 @@
+class Drawdown
+	def self.call(id, amount)
+		response = BankApi.call(id, amount)
+		
+		response.status_code == 200
+	end
+end
+
+
+class BankApi
+	def self.call(id, amount)
+		puts "Makes a call to Barclays to move money"
+		BarclaysClient.make_drawdown(id, amount)
+		# is successful, this word return a ststus code 200, else 4XX or 5XX
+	end
+end
+
+RSpec.describe Drawdown do
+	it 'happy path' do
+		result = Drawdown.call(1111, 10_000)
+		
+		expect(result).to eq true
+		# is there a risk with this test?
+	end
+end
