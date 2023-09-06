3078330962d91b4b61a229d8788f33464bd6cb61
Shen Satkunarasa <shenthuran.satkunarasa@fundingcircle.com>
2023-09-06 16:49:31 +0100

    Passing test with double

--- lib/01.rb
+++ lib/01.rb
@@ -1,23 +1,25 @@
 class Drawdown
-	def self.call(id, amount)
-		response = BankApi.call(id, amount)
+	def self.call(id, amount, bank_api)
+		response = bank_api.call(id, amount)
 		
-		response.status_code == 200
+		response[:status_code] == 200
 	end
 end
 
 
 class BankApi
-	def self.call(id, amount)
+	def call(id, amount)
 		puts "Makes a call to Barclays to move money"
-		BarclaysClient.make_drawdown(id, amount)
+		# BarclaysClient.make_drawdown(id, amount)
 		# is successful, this word return a ststus code 200, else 4XX or 5XX
 	end
 end
 
 RSpec.describe Drawdown do
 	it 'happy path' do
-		result = Drawdown.call(1111, 10_000)
+		bank_api = BankApi.new
+
+		result = Drawdown.call(1111, 10_000, bank_api)
 		
 		expect(result).to eq true
 		# is there a risk with this test?
